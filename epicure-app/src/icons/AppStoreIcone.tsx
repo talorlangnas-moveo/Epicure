b797e0cdbe059ee5eac08e8a6f07fa685710786d
talorlangnas-moveo <talorl@moveo.co.il>
2025-05-25 09:46:22 +0300

    creating icons components
    - AboutUs icon
    -App store icon
    - google play icon

---
+++ epicure-app/src/icons/AppStoreIcone.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function AppStoreIcon() {
+  return (
+    <Image
+      src="/icons/appStoreLogo.svg.svg"
+      width={180}
+      height={52}
+      alt="App Store Icon"
+    />
+  );
+}
---
+++ epicure-app/src/icons/GooglePlayIcon.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function GooglePlayIcon() {
+  return (
+    <Image
+      src="/icons/googlePlayIcon.svg.svg"
+      width={180}
+      height={52}
+      alt="Google Play Icon"
+    />
+  );
+}
