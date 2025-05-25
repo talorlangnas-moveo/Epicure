a36c33503e2541b2224ee8dee4a49a2086f20297
talorlangnas-moveo <talorl@moveo.co.il>
2025-05-25 18:34:33 +0300

    uploading chefs images

--- epicure-app/src/app/page.tsx
+++ epicure-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@components/hero/Hero";
 import Card from "@/components/card/Card";
 import claroImge from "@public/restaurants/claro.png";
 import IconLegend from "@/components/iconLegend/IconLegend";
-import Footer from '@/components/Footer/Footer';
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
