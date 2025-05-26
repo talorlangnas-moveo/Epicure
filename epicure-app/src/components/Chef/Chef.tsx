3d44b59367e167943c1f89085d57e0b90101a056
talorlangnas-moveo <talorl@moveo.co.il>
2025-05-26 10:59:39 +0300

    working on chef component

--- epicure-app/src/components/Chef/Chef.tsx
+++ epicure-app/src/components/Chef/Chef.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./chef.module.scss";
 import { ChefInfo } from "@/types/interfaces/ChefInfo";
 
@@ -9,19 +8,9 @@ interface ChefProps {
 export default function Chef({ chef }: ChefProps) {
   <div className={styles.mainContainer}>
     <h4>chef of the week:</h4>
-    <div className={styles.chefContainer}>
-      <div className={styles.imageWrapper}>
-        <Image
-          src={chef.imgUrl}
-          alt="Example"
-          fill
-          className={styles.image}
-          placeholder="blur"
-        />
-        <div className={styles.footerOverlay}>
-          <p className={styles.footerText}>Your footer text here</p>
-        </div>
-      </div>
+    {chef.img}
+    <div>
+      <p>{chef.description}</p>
     </div>
   </div>;
 }
