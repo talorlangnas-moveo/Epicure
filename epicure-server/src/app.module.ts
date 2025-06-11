f3c1d86ac74c2ca1addac0766674784e93f0fdc8
talorlangnas-moveo <talorl@moveo.co.il>
2025-06-11 19:56:36 +0300

    working on RestaurantsModule - in process

--- epicure-server/src/app.module.ts
+++ epicure-server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI!),
+    RestaurantsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
