33f709a7746ca5b81b0a2e42ffada699a7ce4c57
talorlangnas-moveo <talorl@moveo.co.il>
2025-06-12 19:32:27 +0300

    refactor: move App and Restaurant modules to modules directory

--- epicure-server/src/modules/app/app.module.ts
+++ epicure-server/src/modules/app/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RestaurantsModule } from './restaurants/restaurants.module';
+import { RestaurantsModule } from '../restaurants/restaurants.module';
 
 @Module({
   imports: [
