0e7524a1bafd222c3baff1f6130833ff26f1fe94
talorlangnas-moveo <talorl@moveo.co.il>
2025-06-15 21:15:40 +0300

    feat: add restaurant interface and API call for retrieving all restaurants
    - Introduced Restaurant interface to define the structure of restaurant data.
    - Implemented getAllRestaurants function to fetch restaurant data from the API using axios.

--- epicure-app/src/services/restaurants.ts
+++ epicure-app/src/services/restaurants.ts
@@ -1,3 +1,20 @@
 import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export interface Restaurant {
+  name: string;
+  description: string;
+  imgUrl: string;
+  rating: number;
+  openingTime: string;
+  closingTime: string;
+  foundedDate: Date;
+  createdAt: Date;
+}
+
+export async function getAllRestaurants(): Promise<Restaurant[]> {
+  const res = await axios.get(`${API_URL}/restaurants`);
+  console.log(res.data);
+  return res.data;
+}
