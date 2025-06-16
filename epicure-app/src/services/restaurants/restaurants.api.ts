7e25da55d59a6af137d03cda8b3e56c9eb0d223e
talorlangnas-moveo <talorl@moveo.co.il>
2025-06-16 16:58:35 +0300

    modify restaurant interface to original name name

--- epicure-app/src/services/restaurants/restaurants.api.ts
+++ epicure-app/src/services/restaurants/restaurants.api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Restaurant } from '@/types/interfaces/restaurant_tmp';
+import { Restaurant } from '@/types/interfaces/restaurant';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
