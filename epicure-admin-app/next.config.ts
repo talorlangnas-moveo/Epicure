b4738fdc4b4b45857301820e6465a92be641626d
talorlangnas-moveo <talorl@moveo.co.il>
2025-07-01 16:53:05 +0300

    Removed commented-out code related to dynamic protocol and hostname retrieval for image URLs.

--- epicure-admin-app/next.config.ts
+++ epicure-admin-app/next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        // protocol: process.env.NEXT_PUBLIC_API_BASE_URL?.startsWith('https') ? 'https' : 'http',
         protocol: 'http',
-        // hostname: process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/^https?:\/\//, '').split('/')[0] || 'localhost',
         hostname: 'localhost',
         port: '3000',
         pathname: '/static/**',
