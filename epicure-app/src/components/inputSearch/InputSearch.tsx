5a9aec9273b920188ab2eb33bbd77231ef727314
talorlangnas-moveo <talorl@moveo.co.il>
2025-07-07 18:59:07 +0300

    refactor: enhance InputSearch component with search functionality and new styles
    - Implemented search functionality to fetch restaurants, chefs, and dishes by name.
    - Added new styles for inputSearchContainer and cardsContainer to improve layout.
    - Introduced a debounce mechanism for search input to optimize performance.
    - Updated card styles for search results to ensure consistent presentation.

--- epicure-app/src/components/inputSearch/InputSearch.tsx
+++ epicure-app/src/components/inputSearch/InputSearch.tsx
@@ -1,45 +1,98 @@
 "use client";
 
+import { useEffect, useMemo, useState } from "react";
 import styles from "./inputSearch.module.scss";
 import Image from "next/image";
 import { SearchIcon } from "@icons";
 import clsx from "clsx";
+import { debounce } from "@/utils/utils.functions";
+import Card, { CardInfo } from "@components/card/card";
 
-// export function debounce(func: Function, delay: number) {
-//   let timeout: NodeJS.Timeout;
-//   return function (...args: any[]) {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(() => func.apply(this, args), delay);
-//   };
-// }
+import { getRestaurantsByName } from "@/services/restaurants/restaurants.api";
+import { getRestaurantsAsCards } from "@/services/restaurants/restaurants.utils";
+
+import { getChefsByName } from "@/services/chefs/chefs.api";
+import { convertChefToCard } from "@/services/chefs/chefs.utils";
+
+import { getDishesByName } from "@/services/dishes/dishes.api";
+import { getDishesAsCards } from "@/services/dishes/dishes.utils";
 
 interface InputSearchProps {
   variant?: "default" | "hero";
 }
 
 export default function InputSearch({ variant = "default" }: InputSearchProps) {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<CardInfo[]>([]);
+
+  const [debouncedSetTerm, clearDebouncedTerm] = useMemo(
+    () => debounce((value: unknown) => setQuery(value as string), 1000),
+    []
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    debouncedSetTerm(value);
+  };
+
+  useEffect(() => {
+    if (query) {
+      const fetchResults = async () => {
+        const restaurants = await getRestaurantsByName(query);
+        const chefs = await getChefsByName(query);
+        const dishes = await getDishesByName(query);
+
+        const restaurantsCards = await getRestaurantsAsCards(restaurants);
+        const chefsCards = await Promise.all(chefs.map(convertChefToCard));
+        const dishesCards = await getDishesAsCards(dishes);
+
+        setResults([...restaurantsCards, ...chefsCards, ...dishesCards]);
+      };
+      fetchResults();
+    } else {
+      setResults([]);
+    }
+
+    return () => {
+      clearDebouncedTerm();
+    };
+  }, [query, clearDebouncedTerm]);
 
   return (
-    <div
-      className={clsx(
-        styles.inputSearch,
-        variant === "hero" ? styles.inputSearchHero : styles.inputSearchDefault
-      )}
-    >
-      <div className={styles.searchIcon}>
-        <Image
-          src={SearchIcon}
-          alt="Search Icon"
-          fill
-          style={{ objectFit: "contain" }}
+    <div className={styles.inputSearchContainer}>
+      <div
+        className={clsx(
+          styles.inputSearch,
+          variant === "hero"
+            ? styles.inputSearchHero
+            : styles.inputSearchDefault
+        )}
+      >
+        <div className={styles.searchIcon}>
+          <Image
+            src={SearchIcon}
+            alt="Search Icon"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <input
+          className={styles.inputContainer}
+          id="search-input"
+          type="text"
+          placeholder="Search for restaurant cuisine, chef"
+          value={searchTerm}
+          onChange={handleChange}
         />
       </div>
-      <input
-        className={styles.inputContainer}
-        id="search-input"
-        type="text"
-        placeholder="Search for restaurant cuisine, chef"
-      />
+      <div className={styles.cardsContainer}>
+      {results &&
+        results.map((result) => (
+          <Card key={result.id} {...result} className="searchCard" />
+        ))}
+      </div>
     </div>
   );
 }
